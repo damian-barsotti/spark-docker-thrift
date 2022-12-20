9139684659fe7af4e68f7f8dd90961f077465ebe
Damian Barsotti <damian@famaf.unc.edu.ar>
2022-12-20 03:12:16 -0300

    Fix tipo in folder name

--- files/run-test.sh
+++ files/run-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rm -rf /files/spark-warehose/airports
+rm -rf /files/spark-warehouse/airports
  
 $SPARK_HOME/bin/spark-submit --master spark://spark-master:7077 /files/load_data_write_to_server.py
